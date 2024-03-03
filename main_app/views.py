7a5a33e81f72098b76fc7eff46dcd66bc6e081b0
Berlin <tommyonik@gmail.com>
2024-03-03 18:32:54 -0500

    setup about route and view

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About the Ringventory</h1>')
