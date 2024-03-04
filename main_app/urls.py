c3468cc5975c3179160003653c8b333848da8c73
Berlin <tommyonik@gmail.com>
2024-03-03 23:20:54 -0500

    set up url and template for index rings view (inventory)

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('inventory/', views.ring_index, name='ring-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+class Ring:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+inventory = [
+  Ring('Lolo', 'tabby', 'Kinda rude.', 3),
+  Ring('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Ring('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Ring('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+
 # Add the following import
 from django.http import HttpResponse
 
@@ -9,3 +24,7 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def ring_index(request):
+  return render(request, 'ring/index.html',
+  { 'inventory': inventory })
