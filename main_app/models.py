42d442632552cd5b878b4e5bbe9aa29bdb6cef59
Berlin <tommyonik@gmail.com>
2024-03-04 00:11:36 -0500

    set up Ring model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS= (
+   ('UN', 'Unlisted'),
+   ('L', 'Live'),
+   ('S', 'Sold')
+)
+CONDITION= (
+   ('G','Good'),
+   ('VG','Very Good'),
+   ('P', 'Pristine')
+)
+
+class Ring(models.Model):
+  name = models.CharField(max_length=250)
+  status = models.CharField(
+     choices = STATUS,
+     default=STATUS[0][0]
+  )
+  size = models.IntegerField()
+  metal = models.CharField(max_length=250)
+  stone = models.CharField(max_length=250)
+  condition = models.CharField(
+     choices = STATUS,
+     default=STATUS[0][0]
+  )
+  weight = models.IntegerField()
+  price = models.IntegerField()
+  purchase_point = models.CharField(max_length=250)
+  purchase_date = models.DateField()
+
+  def __str__(self):
+      return f"{self.name}, sz {self.size}, status {self.status}"
+  
