012d874fca1f3986d0e88c143c286faae99f616b
Berlin <tommyonik@gmail.com>
2024-03-05 13:15:29 -0500

    create new ring form and link to nadd ring in inventory

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('inventory/', views.ring_index, name='ring-index'),
   path('inventory/<int:ring_id>', views.ring_detail, name='ring-detail'),
+  path('inventory/create/', views.RingCreate.as_view(), name='ring-create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Ring
 
 
@@ -16,3 +17,7 @@ def ring_index(request):
 def ring_detail(request, ring_id):
   ring = Ring.objects.get(id=ring_id)
   return render(request, 'rings/detail.html', { 'ring': ring })
+
+class RingCreate(CreateView):
+  model = Ring
+  fields = '__all__'
