76c10be49de6ea8d6b8c8720b6a3cc8e9d3f21a9
Berlin <tommyonik@gmail.com>
2024-03-05 22:46:54 -0500

    refactor home view

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('inventory/', views.ring_index, name='ring-index'),
   path('inventory/<int:ring_id>', views.ring_detail, name='ring-detail'),
   path('inventory/create/', views.RingCreate.as_view(), name='ring-create'),
   path('inventory/<int:pk>/update>', views.RingUpdate.as_view(), name='ring-update'),
   path('inventory/<int:pk>/delete>', views.RingDelete.as_view(), name='ring-delete'),
+  path('accounts/', include('django.contrib.auth.urls')),
 ]
